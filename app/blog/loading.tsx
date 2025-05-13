6c3bddfdda41948aea5157137d2a5586a6869a8c
Faisal <zkryfysl@gmail.com>
2025-05-13 17:36:03 +0300

    fix  some bugs

--- app/blog/loading.tsx
+++ app/blog/loading.tsx
@@ -6,7 +6,7 @@ export default function Loading() {
           <div className="w-12 h-12 rounded-full absolute border-4 border-gray-200"></div>
           <div className="w-12 h-12 rounded-full animate-spin absolute border-4 border-blue-500 border-t-transparent"></div>
         </div>
-        <p className="text-gray-600 text-lg font-medium">Loading...</p>
+        {/* <p className="text-gray-600 text-lg font-medium">Loading...</p> */}
       </div>
     </div>
   );
